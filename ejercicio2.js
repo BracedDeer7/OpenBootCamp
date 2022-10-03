25e48896a7bcf3a54a326d6933de92c3bc17e47e
BracedDeer7 <alexaacg18@gmail.com>
2022-10-02 22:08:41 -0500

    second commit

---
+++ ejercicio2.js
@@ -0,0 +1,22 @@
+
+const nombre = "alexis"
+
+const edad  = 25
+
+var eresdesarrollador = ("true")
+
+const hoy =new Date();
+
+console.log(hoy)
+
+const fechanacimiento = new Date("february 18 1997");
+console.log(fechanacimiento)
+
+const librofavorito = {
+titulo: "laombradelviento",
+autor: "carlos ruiz",
+fecha: 2001,
+URL: "https://www.carlosruizzafon.com/es/la-sombra-del-viento/index.ph" ,
+
+}
+
